914d3d58a888f4dcc631e283eff82dd8509a085b
adelbeit <adelbeit@gmail.com>
2020-08-18 01:20:37 -0700

    learning nextjs will start the portfolio after

---
+++ Projects/Portfolio/nextjs-blog/pages/index.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey I like to do things and eat stuff, and sometimes I even sleep.
+        </p>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
