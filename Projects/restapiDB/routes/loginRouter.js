16e5160110a28a9c72f67f58f6700c1e865b0117
adelbeit <adelbeit@gmail.com>
2021-03-04 11:17:16 -0800

    cleanup

--- Projects/restapiDB/routes/loginRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// get login page
-router.get("/", function (req, res) {
-  res.render("login", { title: "Login" });
-});
-
-module.exports = router;
